81fd18c8c0e2e1557cbe287c82fed78869b0b29f
edwinlongjohn <edwinlongjohn@gmail.com>
2023-10-16 11:01:15 +0100

    few changes

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -3,9 +3,9 @@ $('.partner-carousel').slick({
     autoplay: true,
     focusOnSelect: false,
     speed: 1000,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: false,
+    arrows: true,
     prevArrow:"<button type='button' class='slick-prev pull-left'><i class=\"icon-double-left\"  aria-hidden='true'></i></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><i class=\"icon-double-right\"  aria-hidden='true'></i></button>",
     dots: false,
@@ -28,9 +28,37 @@ $('.partner-carousel').slick({
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           infinite: true
         }
       }
     ]
   });
+
+  $('.slider-for').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  // $('.slider-single').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: true,
+  //   fade: false,
+  //   adaptiveHeight: true,
+  //   infinite: false,
+  //  useTransform: true,
+  //   speed: 400,
+  //   cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+  // });
+  // $('.slider-nav').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-for',
+  //   adaptiveHeight: true,
+  //   dots: true,
+  //   centerMode: true,
+  //   focusOnSelect: true
+  // });
