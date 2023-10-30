1ffa65cc2f2960249aa1f2746db71e9398c3d051
edwinlongjohn <edwinlongjohn@gmail.com>
2023-10-30 06:26:11 +0100

    all-fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,11 +19,15 @@ addObserver('.fade-in-up', 'animate__animated', 'animate__fadeInUp')
 $(window).on('scroll', ()=>{
     let scroll = $(window).scrollTop();
     if(scroll < 600){
-        $('.header-nav').removeClass('sticky');
+        $('.header-nav').removeClass('sticky-top');
+        $('.header-nav').removeClass('p-2');
         $('.header-nav .navbar-brand').text('Menu');
+        $('#totop').removeClass('top-visible')
     }else{
-        $('.header-nav').addClass('sticky');
+        $('.header-nav').addClass('sticky-top');
+        $('.header-nav').addClass('p-2');
         $('.header-nav .navbar-brand').html('<a href="index.html" ><img class="img-fluid" src="assets/images/logo.png" alt="logo-img" width="100px" height="100px"/> </a>');
+        $('#totop').addClass('top-visible')
     }
 })
 
@@ -33,3 +37,49 @@ $('.responsive-header-left .search-btn i').on('click', ()=>{
     $('.header-top-responsive').toggleClass('mb-5');    
     $('.header-nav .border').toggleClass('mt-5');
 })
+
+function download(){
+        Swal.fire(
+            'Coming Soon!',
+            '',
+            'info'
+            );
+}
+
+const selectAnswer = $('#answer');
+const selectWho = $('.who');
+const selectWhoTab = $('.who-tab');
+const selectWhoTab1 = $('.who-tab-1');
+selectAnswer.on('change', ()=>{
+    if(selectAnswer.val() === 'no'){
+        $('.no-pop-up-message').css('display', 'block');
+    }else{
+        $('.no-pop-up-message').css('display', 'none');
+    };
+
+})
+
+selectWho.on('change', () =>{
+    console.log(selectWho.val());
+    if(selectWho.val() === 'other'){
+        $('#who-modal').modal('show');
+    }
+})
+selectWhoTab.on('change', () =>{
+    console.log(selectWhoTab.val());
+    if(selectWhoTab.val() === 'other'){
+        $('#who-modal').modal('show');
+    }
+})
+selectWhoTab1.on('change', () =>{
+    console.log(selectWhoTab1.val());
+    if(selectWhoTab1.val() === 'other'){
+        $('#who-modal').modal('show');
+    }
+})
+
+
+//animation 
+setTimeout(() => {
+    $('.loader-bg').fadeToggle();
+}, 1500);
